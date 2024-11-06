7d54049e14c44a3c10701379ca82d099cef09bbc
Zahir Sinancevic <zahir.sinancevic@edu.fit.ba>
2024-11-06 19:52:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GameRoomManagementSystem
+ GameRoomManagementSystem
---
+++ backend/RS1_2024_25.API/Data/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using RS1_2024_25.API.Helper;
+
+namespace RS1_2024_25.API.Data.Models;
+
+public class Country : IMyBaseEntity
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    // public List<City> Cities{ get; set; }
+
+}
